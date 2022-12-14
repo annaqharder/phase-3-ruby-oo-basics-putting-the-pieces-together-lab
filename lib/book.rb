6066b75622c996b7e5c09d2362264d17972c1633
Anna Harder <annaqharder@gmail.com>
2022-12-14 15:22:21 -0700

    commit tree 34e13c9f24a7ce9945dab1be2a6e27db521bf671

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
 
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title, author, page_count, genre)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+b = Book.new("Intro to Ruby", "Stephen", 110, "CS")
+b.author = "Sam"
+puts b.author
 
